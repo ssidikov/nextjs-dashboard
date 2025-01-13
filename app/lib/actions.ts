b7422cdeefbb3291b175c725d9ae8291ee873b2e
Sardorbek SIDIKOV <sardorbek.sidikov7@gmail.com>
2025-01-13 22:57:36 +0100

    Add error handling for invoice operations and implement 404 Not Found page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,16 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
+  try {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
+  } catch (error) {
+    return {
+      message: 'Database error: Failed to create invoice.',
+    }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
@@ -43,17 +49,28 @@ export async function updateInvoice(id: string, formData: FormData) {
   })
   const amountInCents = amount * 100
 
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `
+  } catch (error) {
+    return {
+      message: 'Database error: Failed to update invoice.',
+    }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
-  await sql` DELETE FROM invoices WHERE id = ${id} `
-  revalidatePath('/dashboard/invoices')
+  try {
+    await sql` DELETE FROM invoices WHERE id = ${id} `
+    revalidatePath('/dashboard/invoices')
+    return { message: 'Invoice deleted successfully.' }
+  } catch (error) {
+    return { message: 'Database error: Failed to delete invoice.' }
+  }
 }
